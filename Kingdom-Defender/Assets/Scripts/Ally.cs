bd34e0f57e81f796877b6308975dd30eafc8843f
Reider <trunghieuy2006@gmail.com>
2025-07-22 09:35:41 +0700

    Update: Thêm ShooterAlly kế thừa từ Ally

--- Kingdom-Defender/Assets/Scripts/Ally.cs
+++ Kingdom-Defender/Assets/Scripts/Ally.cs
@@ -3,72 +3,18 @@ using UnityEngine;
 
 public class Ally : MonoBehaviour
 {
-    public GameObject projectile;
-    public float fireRate;
-    public float fireTimer;
-    public float raycastDistance;
-    public Collider2D safeZoneCollider;
-    public LayerMask enemyLayer;
-
     public float health;
     public float currentHealth;
     public bool isPlaced = false;
 
-    void Start()
+    protected virtual void Start()
     {
         currentHealth = health;
-
-        GameObject safeZone = GameObject.FindWithTag("SafeZone");
-        if (safeZone!= null)
-        {
-            safeZoneCollider = safeZone.GetComponent<Collider2D>();
-        }
     }
 
-    void Update()
+    protected virtual void Update()
     {
-        if (!isPlaced)
-        {
-            return;
-        }
-        fireTimer += Time.deltaTime;
 
-        if (fireTimer >= fireRate && ShouldShoot())
-        {
-            Shoot();
-            fireTimer = 0f;
-        }
-    }
-
-    bool ShouldShoot()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, raycastDistance, enemyLayer);
-
-        if (hit.collider != null)
-        {
-            GameObject enemy = hit.collider.gameObject;
-            if (!IsInSafeZone(enemy))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool IsInSafeZone(GameObject enemy)
-    {
-        Collider2D enemyCollider = enemy.GetComponent<Collider2D>();
-        if (safeZoneCollider == null && enemyCollider == null)
-        {
-            return false;
-        }
-
-        return safeZoneCollider.bounds.Intersects(enemyCollider.bounds);
-    }
-
-    void Shoot()
-    {
-        Instantiate(projectile, transform.position, Quaternion.identity);
     }
 
     public void TakeDamage(float damage)
@@ -83,12 +29,5 @@ public class Ally : MonoBehaviour
     void Die()
     {
         Destroy(gameObject);
-    }
-
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * raycastDistance);
     }
 }
